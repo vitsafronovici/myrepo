01a548814d839579241a9f119abdb3517d7cc6e0
Vitalie Safronovici <vitalie.safronovici@harness.io>
2023-09-29 16:22:25 +0300

    teraform

---
+++ terraform/asg/config.tf
@@ -0,0 +1,32 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-east-1"
+  asgName = "vitasg__1"
+}
+
+output "region" {
+	value = "${var.region}"
+}
+
+output "asgName" {
+	value = "choice-demo"
+}
+
+output "myVar" {
+  value = "myValue"
+}
+
+output "loadBalancer" {
+  value = "vit3-elb"
+}
+
+output "prodListener" {
+  value = "arn:aws:elasticloadbalancing:us-east-1:806630305776:listener/app/vit3-elb/59734084df74f16a/e80c02a9f90639e6"
+}
