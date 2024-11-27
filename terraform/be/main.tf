fbf3982b64af01cf290094ac8bc74ad93a7cb04b
Vitalie Safronovici <vitalie.safronovici@harness.io>
2024-11-27 16:04:05 +0200

    Update main.tf

--- terraform/be/main.tf
+++ terraform/be/main.tf
@@ -6,17 +6,17 @@ resource "aws_instance" "ec2_example" {
     ami = "ami-0305d0b03812a425e"
     instance_type = "t2.micro"
     tags = {
-      Name = "EC2 Instance with remote state tag v2"
+      Name = "EC2 Instance with remote state tag v3"
     }
 }
 
 terraform {
   backend "http" {
-    address = "https://ngonzalez.pr2.harness.io/gateway/iacm/api/orgs/default/projects/vit/workspaces/v2/terraform-backend?accountIdentifier=IBhiK0GsTkOJL53cA4gI-A"
+    address = "https://ngonzalez.pr2.harness.io/gateway/iacm/api/orgs/default/projects/vit/workspaces/v3/terraform-backend?accountIdentifier=IBhiK0GsTkOJL53cA4gI-A"
     username = "harness"
-    lock_address = "https://ngonzalez.pr2.harness.io/gateway/iacm/api/orgs/default/projects/vit/workspaces/v2/terraform-backend/lock?accountIdentifier=IBhiK0GsTkOJL53cA4gI-A"
+    lock_address = "https://ngonzalez.pr2.harness.io/gateway/iacm/api/orgs/default/projects/vit/workspaces/v3/terraform-backend/lock?accountIdentifier=IBhiK0GsTkOJL53cA4gI-A"
     lock_method = "POST"
-    unlock_address = "https://ngonzalez.pr2.harness.io/gateway/iacm/api/orgs/default/projects/vit/workspaces/v2/terraform-backend/lock?accountIdentifier=IBhiK0GsTkOJL53cA4gI-A"
+    unlock_address = "https://ngonzalez.pr2.harness.io/gateway/iacm/api/orgs/default/projects/vit/workspaces/v3/terraform-backend/lock?accountIdentifier=IBhiK0GsTkOJL53cA4gI-A"
     unlock_method = "DELETE"
   }
 }
