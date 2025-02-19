9cfa017ddc7c83f1c678a7bed604669fb90fc8bf
Vitalie Safronovici <vitalie.safronovici@harness.io>
2025-02-19 11:39:32 +0200

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2_example" {
     ami = "ami-0305d0b03812a425e"
-    instance_type = "t2.micro"
+    instance_type = "t3.large"
     tags = {
       Name = "EC2 Instance with remote state tag7x"
     }
