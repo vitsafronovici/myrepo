9f3ae25ccafd6de766dfd129a42053112d6dce80
Vitalie Safronovici <vitalie.safronovici@harness.io>
2025-05-29 12:57:40 +0300

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -10,13 +10,5 @@ resource "aws_instance" "ec2_example" {
     }
 }
 
-terraform {
-    backend "s3" {
-        bucket = "vit-s3-bucket"
-        key    = "vit/terraform/remote/s3/terraform.tfstate"
-        region     = "us-east-1"
-        dynamodb_table = "vit-lock"
-    }
-}
 
 
