4dce07209edc56073ff21e94c15b46707294159c
Vitalie Safronovici <vitalie.safronovici@harness.io>
2025-07-31 15:30:34 +0300

    tofu

---
+++ terraform/tofu/providers.tf
@@ -0,0 +1,30 @@
+
+terraform {
+  required_providers {
+    awscc = {
+      source  = "registry.opentofu.org/hashicorp/awscc"
+      version = "~> 0.1"
+    }
+    aws = {
+      source  = "registry.opentofu.org/hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# #Provider and creds
+provider "aws" {
+  region = "us-east-1"
+  alias  = "awsuse1"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias  = "awsuse2"
+}
+
+provider "awscc" {
+  region = "us-east-1"
+  alias  = "awsccuse1"
+}
+
