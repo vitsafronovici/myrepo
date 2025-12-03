458cd155db46969e1fd3bd698d8b219916087b92
Vitalie Safronovici <vitalie.safronovici@harness.io>
2025-12-03 17:02:24 +0200

    Update main.tf

--- test-tofu/main.tf
+++ test-tofu/main.tf
@@ -4,3 +4,15 @@ module "m1" {
   ami = "ami-0305d0b03812a425e"
   instance_type = "t2.micro"
 }
+
+output "env" {
+  value = module.m1.env
+}
+
+output "ami" {
+  value = module.m1.ami
+}
+
+output "instance_type" {
+  value = module.m1.instance_type
+}
